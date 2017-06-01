3ef28d8825243028865b816c07e9615b59f081b3
Myfanwy <mrowlan1@gmail.com>
2017-06-01 08:46:06 -0700

    remove Sac13 Size Comp fish from alljuvtt tag groups

--- R/getReachMovements.R
+++ R/getReachMovements.R
@@ -5,19 +5,6 @@
 #' @details function author: Matt Espe
 #' @export
 
-getReachOneFish = function(x)
-{
-    x = x[order(x$arrival),]
-    # difference between departure 1...n-1 and arrival 2...n
-    ttime = as.numeric(x$arrival[-1]) - as.numeric(x$departure[-nrow(x)])
-    between_stns = x$Station[-nrow(x)] != x$Station[-1]
-    data.frame(depStation = x$Station[-nrow(x)][between_stns],
-               arrStation = x$Station[-1][between_stns],
-               ttime = ttime[between_stns]/ (60*60*24),
-               TagID = x$TagID[1],
-               stringsAsFactors = FALSE)
-}
-
 getReachMovements = function(df)
 {
     do.call(rbind, lapply(split(df, df$TagID), getReachOneFish))
